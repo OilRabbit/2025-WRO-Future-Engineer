b92e002daff76c68f7d64a139810916b6063ec45
africanboy0412 <125363966+donaldtkp@users.noreply.github.com>
2025-07-19 17:04:52 +0800

    pish

--- main/pixy2I2C.cpp
+++
@@ -1,3 +0,0 @@
-#include "pixy2I2C.h"
-
-Pixy2I2C pixy;
--- main/pixy2I2C.h
+++
@@ -1,9 +0,0 @@
-#ifndef PIXY2_H
-#define PIXY2_H
-
-#include <Arduino.h>
-#include <MatrixMiniR4.h>
-#include <Pixy2I2C.h>
-#include <Pixy2CCC.h>
-
-#endif
