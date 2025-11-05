b3d355884538490cd1292b6deebfbf43f26693d6
OilRabbit <kyeliaa@connect.ust.hk>
2025-11-06 02:15:37 +0800

    New function for OC1 (fixed path)

--- main/buttons.cpp
+++ main/buttons.cpp
@@ -41,18 +41,18 @@ void showbtnState(TFT_COLUMN column, int line_number, int text_size, uint16_t te
   static bool finished = false;
   if (is_btn_pressed(TFT_BTN2)) imu_resetYaw();
   if (is_btn_pressed(TFT_BTN3)) tft.clear();
-  if (is_btn_bumped(TFT_BTN1)){
-    finished = false;
-    run = true;
-  }
-  if (run && !finished){
-    // Serial.println("hi");
-    finished = motor_degree_accel(1000, 300, 300, -20);
-    if (finished) {
-      run = false;
-      motor_stop(BRAKE);
-    }
-  }
+  // if (is_btn_bumped(TFT_BTN1)){
+  //   finished = false;
+  //   run = true;
+  // }
+  // if (run && !finished){
+  //   // Serial.println("hi");
+  //   finished = motor_degree_accel(1000, 300, 300, -20);
+  //   if (finished) {
+  //     run = false;
+  //     motor_stop(BRAKE);
+  //   }
+  // }
   // static int nump = 0;
   // nump += is_btn_bumped(TFT_BTN1);
   // String btn_text = String("NumP:") + String(nump);
